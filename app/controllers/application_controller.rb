b3f053731b485396cf666a69ddb3993f98bb8d2f
wkn816 <wkn816@gmail.com>
2020-11-24 22:06:33 +0900

    名前でログイン

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,17 +2,22 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!,except: [:top]
   before_action :configure_permitted_parameters, if: :devise_controller?
   protected
+  
+  
+  def after_sign_in_path_for(resource)
+    lists_path # ログイン後に遷移するpathを設定
+  end
+  
+  def after_sign_out_path_for(resource)
+    root_path # ログアウト後に遷移するpathを設定
+  end
+  
+  private
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:sign_up,keys:[:name])
+    devise_parameter_sanitizer.permit(:sign_up,keys:[:email])
+    devise_parameter_sanitizer.permit(:sign_in,keys:[:name])
+
   end
-
-  def after_sign_in_path_for(resource)
-    lists_path(resource) # ログイン後に遷移するpathを設定
-end
-
-def after_sign_out_path_for(resource)
-    root_path # ログアウト後に遷移するpathを設定
-end
-
 end
